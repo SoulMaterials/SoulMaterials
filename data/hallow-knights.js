5d69322eef4d4966705a7259cf84686642c5ddcf
SoulMaterials <notalent650@gmail.com>
2026-09-17 13:07:07 -0800

    Launch

--- data/hallow-knights.js
+++ data/hallow-knights.js
@@ -11,7 +11,7 @@ window.SSML_CRATE_DATA.push({
     tier: "mythic",
 
     // CHANGE THIS IMAGE URL TO YOUR CRATE IMAGE
-    image: "",
+    image: "https://media.discordapp.net/attachments/1535462023402364938/1550251695710142616/content.png?ex=6aada7e6&is=6aac5666&hm=43a9b041daa0852e12438059610c74b738a535584aac953a0f90011cb676694c&=&format=webp&quality=lossless&width=1280&height=641",
 
     // CHANGE THE CRATE PRICE HERE
     cost: 9000000000000000,
