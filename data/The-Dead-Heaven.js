be6cdaedc007cb14be907ca4e1a402243a65aefd
SoulMaterials <notalent650@gmail.com>
2026-09-21 08:22:09 -0800

    Launch

---
+++ data/The-Dead-Heaven.js
@@ -0,0 +1,118 @@
+window.SSML_CRATE_DATA = window.SSML_CRATE_DATA || [];
+
+window.SSML_CRATE_DATA.push({
+    id: "The-Dead-Heaven",
+
+    name: "T-H-E-D-E-A-D-H-E-A-V-E-N-S",
+
+    tier: "ROYALTY CORRUPTION",
+
+    // CHANGE THIS IMAGE URL TO YOUR CRATE IMAGE
+    image: "https://media.discordapp.net/attachments/1551589687200850101/1551607567656947843/content.png?ex=6ab296a7&is=6ab14527&hm=7259f8016d03aff7abc4ce795993765c74961813d65086ea27c04b625e1750db&=&format=webp&quality=lossless&width=1536&height=683",
+
+    // CHANGE THE CRATE PRICE HERE
+    cost: 12000000000000,
+
+    // CHANGE THE CRATE GLOW COLOR HERE
+    accent: "#ffe2e2",
+
+    desc: "The Golden Crown of Oakhaven did not tarnish from neglect; it rotted from within. What began as a desperate bargain to save a dying kingdom became the permanent bloodline known across the realms as the Royalty of Corruption.",
+
+    roles: [
+
+        // =====================================================
+        // RAREST ROLE
+        // =====================================================
+
+        {
+            id: "alzariel",
+
+            name: "A-L-Z-A-R-I-E-L",
+
+            rarity: "KISHIN⚔️",
+
+            // EXTREMELY HARD TO GET
+            chance: 0.0001,
+
+            // ROLE GLOW COLOR
+            color: "#ffd0e6",
+
+            // VIRTUAL SELL VALUE
+            price: 0,
+
+            effect: "Passive: LIFE STEAL"
+        },
+
+         {
+            id: "lucariel",
+
+            name: "L-U-C-A-R-I-E-L [TOME]",
+
+            rarity: "KISHIN⚔️",
+
+            chance: chance: 0.0009,,
+
+            color: "#f2e7ff",
+
+            price: 0,
+
+            effect: "Passive: Horna's eye's"
+        },
+        {
+            id: "eraelton",
+
+            name: "E-R-A-E-L-I-O-N",
+
+            rarity: "KISHIN⚔️",
+
+            chance: chance: 0.0001,,
+
+            color: "#ead8ff",
+
+            price: 0,
+
+            effect: "Passive: Nornmin Gun's"
+        },
+        {
+            id: "cash",
+
+            name: "💰",
+
+            rarity: "mythic",
+
+            chance: 0.009,
+
+            color: "#48ff00",
+
+            price: 30000000000000,
+
+            effect: "Passive: Fuck load of money"
+        },
+
+
+
+
+        // =====================================================
+        // OTHER HALLOW KNIGHTS ROLES
+        // =====================================================
+
+       
+
+        {
+            id: "A-stciker",
+
+            name: "A sticker",
+
+            rarity: "mythic",
+
+            chance: 78,
+
+            color: "#9b89ff",
+            price: 5,
+
+            effect: "YOU GOT FOOLED BY: MA-DUH"
+            image:  "https://media.discordapp.net/attachments/1535462023402364938/1549256279355039784/6a9867ebc8bbf5fcb73bd524d51052e2.png?ex=6ab1f1d8&is=6ab0a058&hm=bed2049287b05321f945ff40763820f432e53e92ce92c4932b6b1977127665bb&=&format=webp&quality=lossless"
+        }
+
+    ]
+});
---
+++ data/royalty-of-corruption.js
@@ -0,0 +1,133 @@
+window.SSML_CRATE_DATA = window.SSML_CRATE_DATA || [];
+
+window.SSML_CRATE_DATA.push({
+    id: "royalty-of-corruption",
+
+    name: "R-O-Y-A-L-T-Y-O-F-C-O-R-R-U-P-T-I-O-N",
+
+    tier: "ROYALTY CORRUPTION",
+
+    // CHANGE THIS IMAGE URL TO YOUR CRATE IMAGE
+    image: "https://media.discordapp.net/attachments/1535462023402364938/1550538716030705845/5e9a37cc-d889-4cc9-9a5a-3684635f3904.png?ex=6ab1fef5&is=6ab0ad75&hm=2c63f37e262f5173cef3f6eab9b12a877b20a896cb8f1932f6cf721b6acc00fe&=&format=webp&quality=lossless&width=1280&height=641",
+
+    // CHANGE THE CRATE PRICE HERE
+    cost: 10000000,
+
+    // CHANGE THE CRATE GLOW COLOR HERE
+    accent: "#ff0000",
+
+    desc: "The Golden Crown of Oakhaven did not tarnish from neglect; it rotted from within. What began as a desperate bargain to save a dying kingdom became the permanent bloodline known across the realms as the Royalty of Corruption.",
+
+    roles: [
+
+        // =====================================================
+        // RAREST ROLE
+        // =====================================================
+
+        {
+            id: "the-seraphim-excalibur-of-the-hollow-heavens-ethernal-eye-of-divine-judgement",
+
+            name: "👑THE-SERAPHIM-EXCALIBUR-OF-THE-HOLLOW-HEAVENS-ETERNAL-EYE-OF-DIVINE-JUDGEMENT👑",
+
+            rarity: "ULTRA",
+
+            // EXTREMELY HARD TO GET
+            chance: 0.0001,
+
+            // ROLE GLOW COLOR
+            color: "#eca0ff",
+
+            // VIRTUAL SELL VALUE
+            price: 0,
+
+            effect: "KING OF THE SAINT'S"
+        },
+
+         {
+            id: "Rescue",
+
+            name: "Rescue",
+
+            rarity: "KISHIN⚔️",
+
+            chance: chance: 0.0001,,
+
+            color: "#5700bb",
+
+            price: 0,
+
+            effect: "Passive: Nornmin Gun's"
+        },
+
+        {
+            id: "cash",
+
+            name: "💰",
+
+            rarity: "mythic",
+
+            chance: 0.009,
+
+            color: "#48ff00",
+
+            price: 30000000000000,
+
+            effect: "Passive: Fuck load of money"
+        },
+
+        {
+            id: "goldensage",
+
+            name: "👑G-O-L-D-E-N-S-A-G-E👑",
+
+            rarity: "KISHIN⚔️",
+
+            chance: chance: 0.0001,,
+
+            color: "#fff78b",
+
+            price: 0,
+
+            effect: "Passive: Nornmin Gun's"
+        },
+
+         {
+            id: "Untitle-angelic-bloom",
+
+            name: "U-N-T-I-T-L-E-A-N-G-E-L-I-C-B-L-O-O-M",
+
+            rarity: "KISHIN⚔️",
+
+            chance: chance: 0.0001,,
+
+            color: "#fff78b",
+
+            price: 0,
+
+            effect: "Passive: Nornmin Gun's"
+        },
+
+        // =====================================================
+        // OTHER HALLOW KNIGHTS ROLES
+        // =====================================================
+
+       
+
+        {
+            id: "A-stciker",
+
+            name: "A sticker",
+
+            rarity: "mythic",
+
+            chance: 78,
+
+            color: "#ff0000",
+
+            price: 5,
+
+            effect: "Paper"
+        }
+
+    ]
+});
